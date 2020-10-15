7dad583f518d3daf2f7cdc5953a654fd740d3139
Dennis <d.m.m.v.d.vorst@student.tue.nl>
2020-10-15 17:15:09 +0200

    -

--- main.py
+++ main.py
@@ -39,8 +39,10 @@ if not Path(settings.progress_settings.path).exists():
 log = Log(directory=settings.directory_log,
           prefix='log_%i_' % job_id,
           save_log=settings.save_log)
+log.logprint('memory usage (cpu): ' + log.memory_usage())
 
 # setup dataset (ds) and dataloader (dl)
+log.logprint('setting up dataset')
 ds = Dataset(settings.path_dataset, n_max=settings.len_dataset)
 n_train = int(settings.train_pct / 100 * len(ds))
 n_val = int((100 - settings.train_pct) / 100 * len(ds))
@@ -50,20 +52,27 @@ dl_train = DataLoader(ds,
 dl_val = DataLoader(ds,
                     batch_size=settings.batch_size,
                     sampler=SubsetRandomSampler(range(n_train, len(ds))))
+log.logprint('...done, memory usage (cpu): ' + log.memory_usage())
 
 # loss function
 lf = torch.nn.MSELoss()
 
 # model & optimizer
+log.logprint('initializing model...')
 model = Design.get_model_from_name(modelname)
 optimizer = torch.optim.Adam(
     model.parameters(), lr=1e-4
 )
+log.logprint('...done')
 
 # design
+log.logprint('creating design...')
 design = Design().create(
     dl_train, dl_val, model, lf, optimizer, None, settings.epochs, log
 )
+log.logprint('...done')
 
 # start training
+log.logprint('--- training start --- ')
 design.train(settings.progress_settings)
+log.logprint('--- training end ---')
